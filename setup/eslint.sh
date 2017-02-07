c22e28bbe731365a7fa3204b125ba307e37786c0
eakl <elias.ak.lanvin@gmail.com>
2017-02-07 21:28:17 +0100

    Fixed bug on copying ESLint files

--- setup/eslint.sh
+++ setup/eslint.sh
@@ -2,7 +2,7 @@
 
 ESLINT_DIR="${DOTFILES_DIR}/eslint"
 
-if cd $ESLINT_DIR && chmod a+x eslint-bootstrap; then
+if cd "$ESLINT_DIR" && chmod a+x eslint-bootstrap; then
   success "CHMOD +x applied to 'eslint-bootstrap'"
 else
   echo "CHMOD +x has not been applied to 'eslint-bootstrap'";
